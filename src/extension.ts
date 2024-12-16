a6892afb9d9c42314c82effec68be88c5637461a
uniqueFranky <y_r_b@qq.com>
2024-12-16 15:03:06 +0800

    feat: add HistoryWebView

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+var selectionTimeOut: NodeJS.Timeout;
+var historyPanel: vscode.WebviewPanel;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -20,6 +23,54 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	
+	// listen on selection
+	const selectionChangeDisposable = vscode.window.onDidChangeTextEditorSelection((e) => {
+		const selection = e.selections[0];
+		if (selection.isEmpty) {
+			return;
+		}
+
+		// in case of unstable selection
+		if(selectionTimeOut) {
+			clearTimeout(selectionTimeOut);
+		}
+
+		selectionTimeOut = setTimeout(() => {
+			const selectedText = e.textEditor.document.getText(selection);
+			updateHistoryPanel(selectedText);
+		}, 1000);
+
+	});
+	context.subscriptions.push(selectionChangeDisposable);
+
+}
+
+
+function updateHistoryPanel(selectedText: string) {
+	if(!historyPanel) {
+		historyPanel = vscode.window.createWebviewPanel('HistoryPanel', 'HistoryView', vscode.ViewColumn.Beside);
+	}
+	historyPanel.webview.html = getHistoryViewContent(selectedText);
+}
+
+
+function getHistoryViewContent(selectedText: string): string {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Code Viewer</title>
+        </head>
+        <body>
+            <h2>Selected Code:</h2>
+            <pre>${selectedText}</pre>
+            <p>This is your selected code in the editor.</p>
+        </body>
+        </html>
+    `;
 }
 
 // This method is called when your extension is deactivated
